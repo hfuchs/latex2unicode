392179955fa6ececc25ce15a82ecba022ade1679
Hagen Fuchs <hagen.fuchs@physik.tu-dresden.de>
2012-02-15 08:48:38 +0100

    L2U/Object.pm: Add stub.

---
+++ lib/L2U/Object.pm
@@ -0,0 +1,13 @@
+package L2U::Object;
+
+use version; our $VERSION = qv('0.3');
+use warnings; use strict; use utf8;
+use feature qw/switch say/;
+use L2U::Constants;
+use L2U::Handlers;
+
+
+1;
+
+__END__
+bit of doc
